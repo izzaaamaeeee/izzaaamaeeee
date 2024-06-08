10e296b7184af85d4dc3e2627b456d8e2d9da02b
izzaaamaeeee <152254827+izzaaamaeeee@users.noreply.github.com>
2024-06-08 18:27:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,12 @@ btnLike2.addEventListener("click",clickLike2)
 const submit = document.getElementById("submit")
 const comment = document.getElementById("comment")
 const commentbox = document.getElementById("commentbox")
+
 function submitComment(){
+  
   commentbox.textContent += comment.value.toString() + "\n"
   comment.value=""
+  
 }
+
 submit.addEventListener("click",submitComment)
